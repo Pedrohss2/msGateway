5157fed4cd5e1e55e2c777de4f64666d87af2949
Pedro Henrique <pedro2266809ph@gmail.com>
2024-05-31 16:38:16 -0300

    first commit

---
+++ src/main/java/io/github/pedrohss2/msgateway/MsgatewayApplication.java
@@ -0,0 +1,28 @@
+package io.github.pedrohss2.msgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class MsgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator locator(RouteLocatorBuilder builder)  {
+		return builder
+				.routes()
+				.route(r -> r.path("/clientes/**").uri("lb://msclientes"))
+				.build();
+	}
+
+}
