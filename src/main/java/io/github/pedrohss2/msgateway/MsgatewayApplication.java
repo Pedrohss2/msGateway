22fa230b308dab5e82fad0c0cdecfb1a5013b9f9
Pedro Henrique <pedro2266809ph@gmail.com>
2024-06-01 12:30:39 -0300

    feat: adicionando a toda do ms de cartoes

--- src/main/java/io/github/pedrohss2/msgateway/MsgatewayApplication.java
+++ src/main/java/io/github/pedrohss2/msgateway/MsgatewayApplication.java
@@ -22,6 +22,7 @@ public class MsgatewayApplication {
 		return builder
 				.routes()
 				.route(r -> r.path("/clientes/**").uri("lb://msclientes"))
+				.route(r -> r.path("/cartoes/**").uri("lb://mscartoes"))
 				.build();
 	}
 
