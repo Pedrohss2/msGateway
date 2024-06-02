f2170fe39a718f273e8da61becf004219c3ce1c1
Pedro Henrique <pedro2266809ph@gmail.com>
2024-06-02 11:40:35 -0300

    feat: adicionando url para solicitacao de cartao

--- src/main/java/io/github/pedrohss2/msgateway/MsgatewayApplication.java
+++ src/main/java/io/github/pedrohss2/msgateway/MsgatewayApplication.java
@@ -24,6 +24,7 @@ public class MsgatewayApplication {
 				.route(r -> r.path("/clientes/**").uri("lb://msclientes"))
 				.route(r -> r.path("/cartoes/**").uri("lb://mscartoes"))
 				.route(r -> r.path("/avaliacoes-credito/**").uri("lb://msavaliadordecreditos"))
+				.route(r -> r.path("/solicitacao-cartao/**").uri("lb://msavaliadordecreditos"))
 				.build();
 	}
 
